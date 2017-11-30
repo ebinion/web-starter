a64b4680da4ff861af71427a5362c098adeaabd3
Ezekiel Binion <ebinion@gmail.com>
2017-11-30 21:17:08 +0100

    feat: handle JS and CSS with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'assets/styles/[name]-[id].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+})
+
+const config = {
+  entry: {
+    bundle: './source/assets/scripts/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'assets/scripts/[name]-[id].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    extractSass,
+    new HTMLWebpackPlugin({
+      template: './source/index.html'
+    })
+  ]
+}
+
+module.exports = config
